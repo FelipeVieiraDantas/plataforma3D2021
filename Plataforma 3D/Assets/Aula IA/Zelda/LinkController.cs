de31fc25b4d79f4bc5ac1777183cd5e6e49593b8
FelipeDantas <f.vieira.sites@gmail.com>
2021-11-03 10:32:36 -0300

    Interação com baú e inimigo

--- Plataforma 3D/Assets/Aula IA/Zelda/LinkController.cs
+++ Plataforma 3D/Assets/Aula IA/Zelda/LinkController.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class LinkController : MonoBehaviour
 {
     public float velocidade = 10;
     Rigidbody fisica;
     Animator anim;
+
+    public GameObject cinematicaBau;
+    public PlayableDirector diretor;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            cinematicaBau.SetActive(true);
+            other.enabled = false;
+            fisica.isKinematic = true;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         fisica = GetComponent<Rigidbody>();
-        anim = GetComponentInChildren<Animator>();
+        anim = transform.GetChild(0).GetComponentInChildren<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
+        if (cinematicaBau.activeInHierarchy)
+        {
+            if(diretor.state != PlayState.Playing)
+            {
+                cinematicaBau.SetActive(false);
+                anim.transform.localPosition = Vector3.zero;
+                anim.transform.localRotation = Quaternion.Euler(Vector3.zero);
+                fisica.isKinematic = false;
+            }
+            
+            return;
+        }
+
+
+
         float movimentoVertical = Input.GetAxis("Vertical");
         float movimentoHorizontal = Input.GetAxis("Horizontal");
 
